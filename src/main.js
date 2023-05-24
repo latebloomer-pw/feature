6def3c98cdd36ede55e9de6ec69a28147cf6167e
Pete <pete@latebloomer.studio>
2023-05-24 16:50:28 -0400

    checkin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
 import './styles/style.css'
+import Lenis from '@studio-freight/lenis'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+//import $ from 'jquery'
+const lenis = new Lenis()
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
 
-console.log('Welcome to Vite + JS + Webflow!')
+requestAnimationFrame(raf)
 
-createBadge()
-animateTitle()
+gsap.registerPlugin(ScrollTrigger)
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.landing',
+      start: 'bottom +300',
+      scrub: true,
+    },
+  })
+  .from('.logo-overlay', {
+    xPercent: -150,
+    ease: 'slow',
+    delay: 1,
+  })
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: '.case-studies',
+      start: 'bottom +300',
+      scrub: true,
+    },
+  })
+  .from('.big-serif', {
+    xPercent: -150,
+  })
