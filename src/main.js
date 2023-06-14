09399177238843108414cc32896397a33d0a8a67
pete <pete@latebloomer.studio>
2023-06-13 20:18:09 -0400

    checkin

--- src/main.js
+++ src/main.js
@@ -4,58 +4,102 @@ import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 import videoBg from './features/videoSwap'
-//import $ from 'jquery'
 
-const lenis = new Lenis()
+const LINK_ID = '.sidebar-links'
+const links = document.querySelectorAll(LINK_ID)
+gsap.registerPlugin(ScrollTrigger)
+
+// LENIS SETUP
+const lenis = new Lenis({ lerp: 0.05 })
 function raf(time) {
   lenis.raf(time)
   requestAnimationFrame(raf)
 }
 
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000)
+})
+// ===============
+
+// // GSAP ANIMATIONS
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: '.lightbox-link',
+//       start: 'bottom bottom',
+//       scrub: true,
+//     },
+//   })
+//   .from('.homelink', {
+//     scale: 6,
+//     xPercent: 300,
+//     yPercent: -125,
+//   })
+
+// gsap.to('.footer-text-scroll', {
+//   duration: 25,
+//   ease: 'none',
+//   x: '-=1000', //move each box 500px to right
+//   repeat: true,
+// })
+// // ===============
+
+// GET ALL CASE STUDY SECTIONS
+// const getAllCaseStudies = () => {
+//   const pageSections = []
+//   links.forEach((link) => {
+//     pageSections.push(document.getElementById(link.getAttribute('link-to')))
+//   })
+//   return pageSections
+// }
+
+const addAllClickListeners = () => {
+  links.forEach((link) => {
+    const pageSection = document.getElementById(link.getAttribute('link-to'))
+    link.addEventListener('click', () => {
+      Array(document.getElementsByClassName('sidebar-links')).forEach(
+        (clientName) => clientName.classList.remove('selected')
+      )
+      link.classList.add('selected')
+      lenis.scrollTo(pageSection)
+    })
+  })
+}
+
+setInterval(() => videoBg(), 1000)
+addAllClickListeners()
+
 requestAnimationFrame(raf)
-gsap.registerPlugin(ScrollTrigger)
-
-// large logo animation
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: '.landing',
-      start: 'bottom bottom',
-      scrub: true,
-    },
-  })
-  .fromTo(
-    '.scroll-anim',
-    {
-      opacity: 0,
-      stagger: 0.5,
-    },
-    { opacity: 1, stagger: 0.5 }
-  )
-  .from('.logo-overlay', {
-    delay: 1,
-    xPercent: -100,
-  })
-
-// about / contact animation
-gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: '.case-studies',
-      start: 'top +300',
-      scrub: true,
-    },
-  })
-  .from('.talk-block', {
-    xPercent: -150,
-  })
-
-gsap.to('.footer-text-scroll', {
-  duration: 25,
-  ease: 'none',
-  x: '-=1000', //move each box 500px to right
-  repeat: true,
-  scrub: true,
+lenis.on('scroll', () => {
+  ScrollTrigger.update
+  // const caseStudies = getAllCaseStudies()
+  // if (Math.abs(study.getBoundingClientRect().top) < 200) {
+  //   document.querySelectorAll(`[link-to=${sre}]`)
+  // }
 })
 
-videoBg()
+// PAGE SCROLL INTERACTIONS
+// // console.log(e)
+// // let minPos = Number.MAX_SAFE_INTEGER
+// let item
+// caseStudies.forEach((caseStudy) => {
+//   var topPos = Math.abs(caseStudy.getBoundingClientRect().top)
+//   const vh = Math.max(
+//     document.documentElement.clientHeight || 0,
+//     window.innerHeight || 0
+//   )
+//   item = topPos <= vh ? caseStudy : item
+//   // const caseStudyPosition = Math.abs(caseStudy.getBoundingClientRect().top)
+//   // minItem = caseStudyPosition < minPos ? caseStudy : minItem
+//   // minPos = caseStudyPosition < minPos ? caseStudyPosition : minPos
+// })
+// console.log(item)
+
+// links.forEach((link) => {
+//   const pageSection = document.getElementById(link.getAttribute('link-to'))
+//   console.log('bounding', pageSection.getBoundingClientRect())
+
+//   link.addEventListener('click', function handleClick() {
+//     // pageSection.scrollIntoView({ behavior: 'smooth' })
+//   })
+// })
