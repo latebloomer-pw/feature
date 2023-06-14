580bb94e72120a94912e0dc96e38d80e3f7f9cbd
pete <pete@latebloomer.studio>
2023-06-14 16:13:17 -0400

    logger errors

--- src/main.js
+++ src/main.js
@@ -82,13 +82,13 @@ $('.project').each(function () {
       onEnter: () => {
         isDesktop
           ? $('.sidebar-links').removeClass('selected') &&
-          targetElement.addClass('selected')
+            targetElement.addClass('selected')
           : $('.menu-item-current').eq(0).text(triggerElement.attr('menu-name'))
       },
       onEnterBack: () => {
         isDesktop
           ? $('.sidebar-links').removeClass('selected') &&
-          targetElement.addClass('selected')
+            targetElement.addClass('selected')
           : $('.menu-item-current').eq(0).text(triggerElement.attr('menu-name'))
       },
     },
